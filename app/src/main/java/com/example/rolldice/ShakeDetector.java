29f48dbe1eda9d36bbefd032f5755adf5152ef20
Camilo Saavedra <ci.saavedras@gmail.com>
2023-10-14 02:48:08 -0300

    Add shake gesture for roll dices

---
+++ app/src/main/java/com/example/rolldice/ShakeDetector.java
@@ -0,0 +1,70 @@
+package com.example.rolldice;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.FloatMath;
+import android.util.Log;
+
+
+public class ShakeDetector implements SensorEventListener {
+    private static final float SHAKE_THRESHOLD_GRAVITY = 2.7F;
+    private static final int SHAKE_SLOP_TIME_MS = 500;
+    private static final int SHAKE_COUNT_RESET_TIME_MS = 3000;
+
+    private SensorManager sensorManager;
+    private float lastX, lastY, lastZ;
+    private long lastUpdate;
+
+
+    private OnShakeListener mListener;
+    private long mShakeTimestamp;
+    private int mShakeCount;
+
+    public void setOnShakeListener(OnShakeListener listener) {
+        this.mListener = listener;
+    }
+
+    public interface OnShakeListener {
+        void onShake(int count);
+        void rollDices();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // ignore
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                float speed = Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime * 10000;
+
+                if (speed > 800) {
+                    Log.d("ShakeDetector", "Shake detected");
+                    if (mListener != null) {
+                        mListener.onShake(mShakeCount);
+                        mListener.rollDices();
+                    }
+                }
+
+
+                lastX = x;
+                lastY = y;
+                lastZ = z;
+            }
+        }
+    }
+
+}
